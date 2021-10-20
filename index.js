ba656916d2c0e338bc13a94cf27241ed9036e8f6
Sergey Pechinov <spechinov@sfxdx.ru>
2021-10-20 18:38:37 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,124 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const docx = require('docx');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+const { AlignmentType, Document, HeadingLevel, Packer, Paragraph, TextRun, UnderlineType } = docx;
+
+const headerText = (text, textParams) => new Paragraph({
+  alignment: AlignmentType.RIGHT,
+  children: [
+    new TextRun({
+      text,
+      bold: true,
+      font: 'Times New Roman',
+      size: 24,
+      ...textParams,
+    }),
+  ]
+});
+
+app.post('/', async ({ body }, response) => {
+  const {
+    companyName, director, name, position, department, dateDismissal, dateSigning,
+  } = body;
+
+  if (!companyName || !director || !position || !department || !name || !dateDismissal || !dateSigning) {
+    response.status(400).json({
+      fullData: 'companyName, director, position, department, name, dateDismissal, dateSigning',
+    });
+  }
+
+  const doc = new Document({
+    creator: "Clippy",
+    title: "Sample Document",
+    description: "A brief example of using docx",
+    sections: [{
+      children: [
+        headerText(`Директору ${companyName}`),
+        headerText(director),
+        headerText('от'),
+        headerText(`${name},`),
+        headerText(`${position},`),
+        headerText(department),
+        // headerText('(ФИО, должность и подразделение)', {
+        //   size: 16,
+        //   italics: true,
+        //   bold: false,
+        // }),
+        new Paragraph({}),
+        new Paragraph({}),
+        new Paragraph({}),
+        new Paragraph({}),
+        new Paragraph({}),
+        new Paragraph({}),
+        new Paragraph({}),
+        new Paragraph({}),
+        new Paragraph({}),
+        new Paragraph({}),
+        new Paragraph({}),
+        new Paragraph({
+          alignment: AlignmentType.CENTER,
+          children: [
+            new TextRun({
+              size: 24,
+              text: 'ЗАЯВЛЕНИЕ',
+              bold: true,
+              font: 'Times New Roman',
+            })
+          ]
+        }),
+        new Paragraph({}),
+        new Paragraph({
+          alignment: AlignmentType.CENTER,
+          children: [
+            new TextRun({
+              size: 24,
+              text: `Прошу уволить меня по собственному желанию  ${dateDismissal}`,
+              font: 'Times New Roman',
+            })
+          ]
+        }),
+        new Paragraph({}),
+        new Paragraph({}),
+        new Paragraph({}),
+        new Paragraph({
+          alignment: AlignmentType.LEFT,
+          children: [
+            new TextRun({
+              size: 24,
+              text: `Дата: ${dateSigning}`,
+              font: 'Times New Roman',
+              italics: true,
+            })
+          ]
+        }),
+        new Paragraph({}),
+        new Paragraph({
+          alignment: AlignmentType.LEFT,
+          children: [
+            new TextRun({
+              size: 24,
+              text: 'Подпись: ______________',
+              font: 'Times New Roman',
+              italics: true,
+            })
+          ]
+        }),
+      ],
+    }],
+  });
+  const b64string = await Packer.toBase64String(doc);
+
+  response.setHeader('Content-Disposition', 'attachment; filename=My Document.docx');
+  response.send(Buffer.from(b64string, 'base64'));
+})
+
+app.listen(PORT, () => console.log(`Server is started on ${PORT} port!`));
